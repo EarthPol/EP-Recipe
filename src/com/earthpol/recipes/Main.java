1ec0a7f3e643e1dcef33294adf7d839f61b30edc
0xBit <73406588+0xBit-MC@users.noreply.github.com>
2021-01-09 19:04:12 -0800

    Update Custom Recipes
    Adds Dupe for Carpets,
    
    Warped Nylium
    Crimson Nylim
    Netherrack
    Blackstone (Chisled)

--- src/com/earthpol/recipes/Main.java
+++ src/com/earthpol/recipes/Main.java
@@ -137,7 +137,7 @@ public class Main extends JavaPlugin {
         HeartOfSea.setIngredient('S', Material.SLIME_BALL);
         Bukkit.addRecipe(HeartOfSea);
         
-        //Custom Nether War
+        //Custom Nether Wart
         final ShapedRecipe netherWart = new ShapedRecipe(new NamespacedKey(this, "custom_netherWart"), new ItemStack(Material.NETHER_WART, 8));
         netherWart.shape("RRR", "RBR", "RRR");
         netherWart.setIngredient('R', Material.REDSTONE_BLOCK);
@@ -158,14 +158,140 @@ public class Main extends JavaPlugin {
         Bukkit.addRecipe(soulsand);
         
         //Custom Black Dye
-        final ShapelessRecipe blackdye1 = new ShapelessRecipe(new NamespacedKey(this, "custom_blackdye_1"), new ItemStack(Material.BLACK_DYE, 1));
-        string.addIngredient(1, Material.COAL);
+        final ShapelessRecipe blackdye1 = new ShapelessRecipe(new NamespacedKey(this, "custom_blackdye_1"), new ItemStack(Material.INK_SAC, 1));
+        blackdye1.addIngredient(1, Material.COAL);
         Bukkit.addRecipe(blackdye1);
         
         //Custom Black Dye #2
-        final ShapelessRecipe blackdye2 = new ShapelessRecipe(new NamespacedKey(this, "custom_blackdye_2"), new ItemStack(Material.BLACK_DYE, 1));
-        string.addIngredient(1, Material.CHARCOAL);
+        final ShapelessRecipe blackdye2 = new ShapelessRecipe(new NamespacedKey(this, "custom_blackdye_2"), new ItemStack(Material.INK_SAC, 1));
+        blackdye2.addIngredient(1, Material.CHARCOAL);
         Bukkit.addRecipe(blackdye2);
+        
+        //Custom Sponge
+        final ShapedRecipe sponge = new ShapedRecipe(new NamespacedKey(this, "custom_sponge"), new ItemStack(Material.WET_SPONGE, 8));
+        sponge.shape("WWW","WBW","WWW");
+        sponge.setIngredient('W', Material.YELLOW_WOOL);
+        sponge.setIngredient('B', Material.WATER_BUCKET);
+        Bukkit.addRecipe(sponge);
+        
+        //Custom Blackstone
+        final ShapedRecipe blackstone = new ShapedRecipe(new NamespacedKey(this, "custom_blackstone"), new ItemStack(Material.BLACKSTONE, 8));
+        blackstone.shape("SSS","SDS","SSS");
+        blackstone.setIngredient('S', Material.STONE);
+        blackstone.setIngredient('D', Material.BLACK_DYE);
+        Bukkit.addRecipe(blackstone);
+        
+        
+        //Custom Chiseled Polished Blackstone
+        final ShapedRecipe chiseledBlackstone = new ShapedRecipe(new NamespacedKey(this, "custom_chiseled_blackstone"), new ItemStack(Material.CHISELED_POLISHED_BLACKSTONE, 4));
+        chiseledBlackstone.shape(" BB", " BB", "   ");
+        chiseledBlackstone.setIngredient('B', Material.POLISHED_BLACKSTONE_BRICKS);
+        Bukkit.addRecipe(chiseledBlackstone);
+        
+        //Netherrack
+        final ShapedRecipe netherrack = new ShapedRecipe(new NamespacedKey(this, "custom_netherrack"), new ItemStack(Material.NETHERRACK, 8));
+        netherrack.setIngredient('S', Material.STONE);
+        netherrack.setIngredient('D', Material.RED_DYE);
+        Bukkit.addRecipe(netherrack);
+        
+        //Crimson Nylium
+        final ShapelessRecipe crimsonNylium = new ShapelessRecipe(new NamespacedKey(this, "custom_crimson_nylium"), new ItemStack(Material.CRIMSON_NYLIUM, 1));
+        crimsonNylium.addIngredient(1, Material.MYCELIUM);
+        crimsonNylium.addIngredient(1, Material.NETHER_WART);
+        crimsonNylium.addIngredient(1, Material.NETHERRACK);
+        Bukkit.addRecipe(crimsonNylium);
+        
+        //Warped Nylium
+        final ShapelessRecipe warpedNylium = new ShapelessRecipe(new NamespacedKey(this, "custom_warped_nylium"), new ItemStack(Material.WARPED_NYLIUM, 1));
+        warpedNylium.addIngredient(1, Material.MYCELIUM);
+        warpedNylium.addIngredient(1, Material.NETHER_WART);
+        warpedNylium.addIngredient(1, Material.SOUL_SAND);
+        Bukkit.addRecipe(warpedNylium);
+        
+        
+        /*=================
+         * CARPET COLORS
+         ==================*/
+        
+        // BLACK CARPET
+        final ShapelessRecipe blackCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_black_carpet"), new ItemStack(Material.BLACK_CARPET, 3));
+        blackCarpet.addIngredient(1, Material.BLACK_CARPET);
+        Bukkit.addRecipe(blackCarpet);
+        
+        //Blue Carpet
+        final ShapelessRecipe blueCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_blue_carpet"), new ItemStack(Material.BLUE_CARPET, 3));
+        blueCarpet.addIngredient(1, Material.BLUE_CARPET);
+        Bukkit.addRecipe(blueCarpet);
+        
+        //Brown Carpet
+        final ShapelessRecipe brownCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_brown_carpet"), new ItemStack(Material.BROWN_CARPET, 3));
+        brownCarpet.addIngredient(1, Material.BROWN_CARPET);
+        Bukkit.addRecipe(brownCarpet);
+        
+        //Cyan Carpet
+        final ShapelessRecipe cyanCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_cyan_carpet"), new ItemStack(Material.CYAN_CARPET, 3));
+        cyanCarpet.addIngredient(1, Material.CYAN_CARPET);
+        Bukkit.addRecipe(cyanCarpet);
+        
+        //Gray carpet
+        final ShapelessRecipe grayCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_gray_carpet"), new ItemStack(Material.GRAY_CARPET, 3));
+        grayCarpet.addIngredient(1, Material.GRAY_CARPET);
+        Bukkit.addRecipe(grayCarpet);
+        
+        //Green Carpet
+        final ShapelessRecipe greenCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_green_carpet"), new ItemStack(Material.GREEN_CARPET, 3));
+        greenCarpet.addIngredient(1, Material.GREEN_CARPET);
+        Bukkit.addRecipe(greenCarpet);
+        
+        //lightblue carpet
+        final ShapelessRecipe lightBlueCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_light_blue_carpet"), new ItemStack(Material.LIGHT_BLUE_CARPET, 3));
+        lightBlueCarpet.addIngredient(1, Material.LIGHT_BLUE_CARPET);
+        Bukkit.addRecipe(lightBlueCarpet);
+        
+        //LIGHT GRAY carpet
+        final ShapelessRecipe lightGrayCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_light_gray_carpet"), new ItemStack(Material.LIGHT_GRAY_CARPET, 3));
+        lightGrayCarpet.addIngredient(1, Material.LIGHT_GRAY_CARPET);
+        Bukkit.addRecipe(lightGrayCarpet);
+        
+        //LIME carpet
+        final ShapelessRecipe limeCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_lime_carpet"), new ItemStack(Material.LIME_CARPET, 3));
+        limeCarpet.addIngredient(1, Material.LIME_CARPET);
+        Bukkit.addRecipe(limeCarpet);
+        
+        //Magenta Carpet
+        final ShapelessRecipe magentaCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_magenta_carpet"), new ItemStack(Material.MAGENTA_CARPET, 3));
+        magentaCarpet.addIngredient(1, Material.MAGENTA_CARPET);
+        Bukkit.addRecipe(magentaCarpet);
+        
+        //Pink Carpet
+        final ShapelessRecipe pinkCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_pink_carpet"), new ItemStack(Material.PINK_CARPET, 3));
+        pinkCarpet.addIngredient(1, Material.PINK_CARPET);
+        Bukkit.addRecipe(pinkCarpet);
+        
+        //Purple Carpet
+        final ShapelessRecipe purpleCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_purple_carpet"), new ItemStack(Material.PURPLE_CARPET, 3));
+        purpleCarpet.addIngredient(1, Material.PURPLE_CARPET);
+        Bukkit.addRecipe(purpleCarpet);
+        
+        //Red Carpet
+        final ShapelessRecipe redCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_red_carpet"), new ItemStack(Material.RED_CARPET, 3));
+        redCarpet.addIngredient(1, Material.RED_CARPET);
+        Bukkit.addRecipe(redCarpet);
+        
+        //White Carpet
+        final ShapelessRecipe whiteCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_red_carpet"), new ItemStack(Material.WHITE_CARPET, 3));
+        whiteCarpet.addIngredient(1, Material.WHITE_CARPET);
+        Bukkit.addRecipe(whiteCarpet);
+        
+        //Yellow Carpet
+        final ShapelessRecipe yellowCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_red_carpet"), new ItemStack(Material.YELLOW_CARPET, 3));
+        yellowCarpet.addIngredient(1, Material.YELLOW_CARPET);
+        Bukkit.addRecipe(yellowCarpet);
+        
+        //Orange Carpet
+        final ShapelessRecipe orangeCarpet = new ShapelessRecipe(new NamespacedKey(this, "custom_orange_carpet"), new ItemStack(Material.ORANGE_CARPET, 3));
+        orangeCarpet.addIngredient(1, Material.ORANGE_CARPET);
+        Bukkit.addRecipe(orangeCarpet);
         
         //Custom Blaze Rods
         this.getServer().addRecipe(new FurnaceRecipe(new ItemStack(Material.BLAZE_ROD, 1), Material.IRON_BARS));
